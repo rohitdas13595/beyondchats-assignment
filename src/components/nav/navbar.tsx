3752641a85aa20a2dfe22c3c87460fe7f09042ff
rohitdas13595 <rohitdas10595@gmail.com>
2025-01-29 23:37:18 +0530

    feat:landing page design

---
+++ src/components/nav/navbar.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export function Navbar() {
+  return (
+    <div className="flex flex-row justify-between w-full bg-transparent z-50 items-center sticky top-0 py-2  px-8">
+      <div className="flex items-center  gap-2 justify-center">
+        <Image src="/images/logo.png" alt="logo" width={50} height={50} />
+        <h1 className="text-3xl font-bold text-white">Beyondchats</h1>
+      </div>
+      <div></div>
+      <button className="bg-crust py-1 px-4  font-bold text-white  rounded-2xl text-white shadow-xl border border-white hover:bg-mantle">
+        Create Chat Bot
+      </button>
+    </div>
+  );
+}
